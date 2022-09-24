6be2e881269d1d4b21531aefd38590329fce5e8b
inspirewh <78182677+inspirewh@users.noreply.github.com>
2022-09-24 19:20:30 +0800

    schema edits

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,32 +1,34 @@
-INSERT INTO departments (`id`, `name`) 
-VALUES 
-(1, 'Marketing'),
-(2, 'HR'),
-(3, 'Finance'),
-(4, 'Sales'),
-(5, 'IT');
+USE db_employee_cms;
 
-INSERT INTO roles (`id`, `title`, `salary`, `department_id`) 
+INSERT INTO departments (name) 
 VALUES 
-('1', 'Software Engineer', '110000', 5),
-('2', 'Full-Stack Developer', '110000', 5),
-('3', 'Digital Content Marketer', '60000', 1),
-('4', 'Sales Consultant', '60000', 4),
-('5', 'Accountant', '65000', 3),
-('6', 'Sales Associate', '80000', 4),
-('7', 'Finance Analyst', '80000', 3),
-('8', 'Human Resources Coordinator', '80000', 2),
-('9', 'Human Resources Assistant', '65000', 2),
-('10', 'Marketing Manager', '65000', 1),
+('Marketing'),
+('HR'),
+('Finance'),
+('Sales'),
+('IT');
 
-INSERT INTO employees (`id, first_name, last_name, roles_id, manager_id`)
+INSERT INTO roles (title, salary, department_id) 
+VALUES 
+('Software Engineer', '110000', 5),
+('Full-Stack Developer', '110000', 5),
+('Digital Content Marketer', '60000', 1),
+('Sales Consultant', '60000', 4),
+('Accountant', '65000', 3),
+('Sales Associate', '80000', 4),
+('Finance Analyst', '80000', 3),
+('Human Resources Coordinator', '80000', 2),
+('Human Resources Assistant', '65000', 2),
+('Marketing Manager', '65000', 1),
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
-(001, "John", "Smith", 001, NULL),
-(002, "Michelle", "Williams", 002, NULL),
-(003, "Steve", "Watts", 003, NULL),
-(004, "Ryan", "Jefferson", 004, NULL),
-(005, "Jessica", "James", 005, 001),
-(006, "Linda", "Wood", 006, 002),
-(007, "Roger", "Doe", 007, 003),
-(008, "Darren", "Ryan", 008, 004);
+("John", "Smith", 1, NULL),
+("Michelle", "Williams", 2, NULL),
+("Steve", "Watts", 3, NULL),
+("Ryan", "Jefferson", 4, NULL),
+("Jessica", "James", 5, 1),
+("Linda", "Wood", 6, 2),
+("Roger", "Doe", 7, 3),
+("Darren", "Ryan", 8, 4);
 
